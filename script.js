772e1e6bed5dc04fd553fb891367b82533fec2de
MDMwakulo <mdmwakulo@gmail.com>
2023-06-19 11:58:35 +0300

    Maps: Fundamentals

--- script.js
+++ script.js
@@ -55,8 +55,40 @@ const restaurant = {
   },
 };
 
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+rest.set(2, 'Lisbon, Portugal');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
+// rest.clear();
+console.log(rest.size);
+const arr = [1, 2];
+rest.set(arr, 'Test');
+console.log(rest.get(arr));
+
+rest.set(document.querySelector('h1'), 'Heading');
+/*
 // /////////////////////////////////////////////////////////////////////
-// Coding Challenge #2
+// Sets
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -132,6 +164,8 @@ console.log(
 const myName = new Set('durellmwakulomwawasi');
 console.log(myName);
 console.log(new Set('durellmwakulomwawasi').size);
+*/
+
 /*
 // /////////////////////////////////////////////////////////////////////
 // Coding Challenge #2
