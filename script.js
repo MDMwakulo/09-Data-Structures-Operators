5acce2f9c816eb7d13d2d8e466be39a70d489f61
MDMwakulo <mdmwakulo@gmail.com>
2023-06-20 17:53:09 +0300

    Working with Strings – Part 3

--- script.js
+++ script.js
@@ -54,6 +54,59 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+console.log(`a+very+nice+string`.split('+'));
+console.log('Durell Mwakulo Mwawasi'.split(' '));
+
+const [firstName, , lastName] = 'Durell Mwakulo Mwawasi'.split(' ');
+console.log(firstName, lastName);
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const newArr = [];
+
+  for (const n of names) {
+    // newArr.push(n[0].toUpperCase() + n.slice(1));
+    newArr.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  return console.log(newArr.join(' '));
+};
+
+// Working with Strings – Part 3
+capitalizeName('jessica ann smith davis');
+capitalizeName('durell mwakulo');
+
+// Padding
+const message = `Go to gate 23!`;
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Durell'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(64637384));
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard('3444567687876556787654'));
+
+// Repeat
+const message2 = 'Bad weather... All Departures Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'✈'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+/*
+// /////////////////////////////////////////////////////////////////
 // Working with Strings – Part 2
 const airline = 'TAP Air Portugal';
 
@@ -111,6 +164,7 @@ const checkBaggage = function (items) {
 checkBaggage('I have a laptop, some Food and a pocket Knife');
 checkBaggage('Socks and camera');
 checkBaggage('Got some snacks and a gun for protection');
+*/
 
 /*
 // /////////////////////////////////////////////////////////////////////
